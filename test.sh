685bc51fac5922ff93a7c69bfbd6456797488027
tonyleekorea <42961200+aiegoo@users.noreply.github.com>
2024-09-05 05:23:04 +0900

    add test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
-#
-!/bin/bash
+#!/bin/bash
 
 # check for directory and print a message if it doesn't exist
-if [ ! -d /tmp/test ]; then
+if [ ! -d ../test ]; then
     echo "Directory /tmp/test does not exist."
 fi
+
+# find all pdf files in the directory and print the file names
+for file in $(find ../docs -name "*.pdf"); do
+    echo $file
+done
